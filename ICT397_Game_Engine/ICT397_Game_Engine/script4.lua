a38fdf85f4dc3b546fded2ebf5c61719c9ab91e3
MRXXX24111974 <31859413@student.murdoch.edu.au>
2016-04-19 00:49:19 +0800

    added camera/lua

--- ICT397_Game_Engine/ICT397_Game_Engine/script4.lua
+++ ICT397_Game_Engine/ICT397_Game_Engine/script4.lua
@@ -1,13 +1,21 @@
 --luatest4.lua
 --example of exposing C++ function
 
-multiply=cpp_Multiply(5,7);
+cpp_Add("player", 0, 1, 2, 3, 4, 5, 6, "again", "WOW", 89, true, false);
+cpp_Add1("npc", 1, 1, 2, 3, 4, 5, 6, "again", "WOW", 92, true, false);
+cpp_Add2("npc", 2, 1, 2, 3, 4, 5, 6, "again", "WOW", 92, true, false);
+cpp_Add3("npc", 3, 1, 2, 3, 4, 5, 6, "again", "WOW", 92, true, false);
+cpp_Add4("npc", 4, 1, 2, 3, 4, 5, 6, "again", "WOW", 92, true, false);
+cpp_Add5("player", 5, 1, 2, 3, 4, 5, 6, "again", "WOW", 92, true, false);
+cpp_Add6("player", 6, 1, 2, 3, 4, 5, 6, "again", "WOW", 92, true, false);
+cpp_Add7("player", 7, 1, 2, 3, 4, 5, 6, "again", "WOW", 92, true, false);
+cpp_Add8("player", 8, 1, 2, 3, 4, 5, 6, "again", "WOW", 92, true, false);
+cpp_Add9("player", 9, 1, 2, 3, 4, 5, 6, "again", "WOW", 92, true, false);
+cpp_Add10("player", 10, 1, 2, 3, 4, 5, 6, "again", "WOW", 92, true, false);
+cpp_Add11("player", 11, 1, 2, 3, 4, 5, 6, "again", "WOW", 92, true, false);
+cpp_Add12("player", 12, 1, 2, 3, 4, 5, 6, "again", "WOW", 92, true, false);
+cpp_Add13("player", 13, 1, 2, 3, 4, 5, 6, "again", "WOW", 92, true, false);
+cpp_Add14("player", 14, 1, 2, 3, 4, 5, 6, "again", "WOW", 92, true, false);
+cpp_Add15("player", 15, 1, 2, 3, 4, 5, 6, "again", "WOW", 92, true, false);
 
-add=cpp_Add(3,2);
-
-print("Multiplication = "..multiply);
-print("Adding = "..add);
-
---print(cpp_Multiply(5,6));
---another way of writing it
 
